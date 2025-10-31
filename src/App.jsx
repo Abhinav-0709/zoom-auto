01df8c451a3f167bf31701b48f247e28aa9cf60f
Abhinav-0709 <mrxsahil247@gmail.com>
2025-10-31 12:04:37 +0530

    Updated feature or fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
 import Contact from './components/Contact'
-import "../src/app.css"
+import './App.css'
 import Footer from './components/Footer'
 import Testimonials from './components/Testimonials'
 import { Element } from 'react-scroll';
